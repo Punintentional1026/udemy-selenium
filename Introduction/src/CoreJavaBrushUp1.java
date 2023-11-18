46310975ad76a1dc8dd73850e0de5f638ec0826c
Rusty Horner <RustyHorner@gmail.com>
2023-11-18 17:36:44 -0500

    Course Section 3 - Video 21. Java Arrays

--- Introduction/src/CoreJavaBrushUp1.java
+++ Introduction/src/CoreJavaBrushUp1.java
@@ -13,6 +13,17 @@ public class CoreJavaBrushUp1 {
 		
 		System.out.println(myNum + " is the value stored in the myNum variable");
 		System.out.println(website);
+		//Arrays - 
+		int[] arr = new int[5];
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		arr[4] = 5;
+		
+		int[] arr2 = { 1, 2, 3, 4, 5 };
+		
+		System.out.println(arr2[0]);
 	}
 
 }
