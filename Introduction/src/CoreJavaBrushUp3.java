e1708517b8e23e49ce558a6672a94851cf00003c
Rusty Horner <RustyHorner@gmail.com>
2023-11-24 15:49:06 -0500

    Course Section 3 - Video 27 - Strings in Java - How to declare Strings & Important String methods

---
+++ Introduction/src/CoreJavaBrushUp3.java
@@ -0,0 +1,28 @@
+
+public class CoreJavaBrushUp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//String is an object //String literal
+		//If two or more string literals have the same value only 1 object is created
+		//String s1 = "Rahul Shetty Academy";
+		String s5 = "hello";
+		
+		//new
+		String s2 = new String("Welcome");
+		String s3 = new String("Welcome");
+		
+		String s = "Rahul Shetty Academy";
+		String[] splittedString = s.split("Shetty");
+		System.out.println(splittedString[0]);
+		System.out.println(splittedString[1]);
+		System.out.println(splittedString[1].trim());
+		
+		for(int i = s.length() - 1; i >= 0; i--)
+		{
+			System.out.println(s.charAt(i));
+		}
+	}
+
+}
