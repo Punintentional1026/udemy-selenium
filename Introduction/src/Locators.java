1d33f24009171987a87f85196d4104775f191069
Rusty Horner <RustyHorner@gmail.com>
2023-11-25 17:24:18 -0500

    Course Section 5 - Video 35 - Browser plugins- Selectorshub to identify and validate the elements on the page

--- Introduction/src/Locators.java
+++ Introduction/src/Locators.java
@@ -1,3 +1,5 @@
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,10 +8,11 @@ public class Locators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// implicit wait - 2 seconds time out
+		
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
 		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
