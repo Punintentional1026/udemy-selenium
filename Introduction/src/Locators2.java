50f8c54626e3c4e6a119d01fafdfc78c13c27fec
Rusty Horner <RustyHorner@gmail.com>
2023-11-29 21:34:50 -0500

    Course Section 6 - Video 42 - Generating xpaths based on the button texts on the page with the example

--- Introduction/src/Locators2.java
+++ Introduction/src/Locators2.java
@@ -21,6 +21,8 @@ public class Locators2 {
 		System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
+		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
+		driver.quit();
 	}
 
 }
