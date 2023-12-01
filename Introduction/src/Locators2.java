2954ce013ae2a1f55e5a0298adad452df13bdc5d
Rusty Horner <RustyHorner@gmail.com>
2023-11-30 19:50:56 -0500

    Course Section 6 - Video 43 - Parse the String with Java methods to get the password dynamically from the page

--- Introduction/src/Locators2.java
+++ Introduction/src/Locators2.java
@@ -13,9 +13,10 @@ public class Locators2 {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password = getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
 		Thread.sleep(2000);
 		System.out.println(driver.findElement(By.tagName("p")).getText());
@@ -23,6 +24,25 @@ public class Locators2 {
 		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello " + name + ",");
 		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
 		driver.quit();
+	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		//Please use temporary password 'rahulshettyacademy' to Login.
+		String[] passwordArray = passwordText.split("'");
+		//String[] passwordArray2 = passwordArry[1].split("'");
+		String password = passwordArray[1].split("'")[0];
+		return password;
+		//0th index - Please use temporary password 
+		//1st index - rahulshettyacademy' to Login.
+		
+		//0th index - rahulshettyacademy
+		//1st index -  to Login.
 	}
 
 }
