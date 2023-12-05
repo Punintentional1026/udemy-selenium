e440387b25cd460598657d5d2a16011080cf4271
Rusty Horner <RustyHorner@gmail.com>
2023-12-04 20:46:54 -0500

    Course Section 7 - Video 55 - Handle Dynamic dropdowns with Webdriver API

---
+++ Introduction/src/Dropdown.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dropdown {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		//	//a[@value='BLR'] - xpath for Bengaluru
+		//	//a[@value='MAA'] - xpath for Chennai
+
+		Thread.sleep(2000);
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		
+		driver.quit();
+	}
+
+}
