eb352ee39ca882d69797ca9b40a5025b747f501e
Rusty Horner <RustyHorner@gmail.com>
2023-12-04 21:07:02 -0500

    Course Section 7 - Video 56 - Parent-Child relationship locator to Identify the objects Uniquely

--- Introduction/src/Dropdown.java
+++ Introduction/src/Dropdown.java
@@ -10,15 +10,20 @@ public class Dropdown {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.manage().window().fullscreen();
 
 		//	//a[@value='BLR'] - xpath for Bengaluru
 		//	//a[@value='MAA'] - xpath for Chennai
 
+		//	//div[@id='ctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']
+		//	//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']
+		
 		Thread.sleep(2000);
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		driver.findElement(By.xpath("//a[@value='BLR']")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		//driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 		
 		driver.quit();
 	}
