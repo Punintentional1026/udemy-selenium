76755cd1a31bd2d06cd52355c458eb22a61c70bd
Rusty Horner <RustyHorner@gmail.com>
2023-12-07 19:38:34 -0500

    Section 7: Assignment 1 - Checkboxes exercise

---
+++ Introduction/src/Section7Assignment1.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Section7Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertTrue(driver.findElement(By.id("checkBoxOption1")).isSelected());
+		driver.findElement(By.id("checkBoxOption1")).click();
+		Assert.assertFalse(driver.findElement(By.id("checkBoxOption1")).isSelected());
+
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		
+		driver.quit();
+	}
+
+}
