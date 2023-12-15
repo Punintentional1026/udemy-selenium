4bf7523f7bfd5a248ce0f426e16d2fb1dd39aaff
Rusty Horner <RustyHorner@gmail.com>
2023-12-15 17:29:17 -0500

    Course Section 8 - Video 74. Debugging the code to understand how flow is being built

--- Introduction/src/base.java
+++ Introduction/src/base.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class base {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
@@ -16,24 +16,35 @@ public class base {
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		
+		Thread.sleep(3000L);
+		
 		String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot"}; 
 		
-		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));)
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+
+		int j = 0;
 
 		for (int i=0; i < products.size(); i++)
 		{
-			String name = products.get(i).getText();
+			//Brocolli - 1 Kg
+			String[] name = products.get(i).getText().split("-");
+			String formattedName = name[0].trim();
 			// format it to get actual vegetable name
 			// convert array into array list for easy search
 			// check whether name you extracted is present in array or not
 			
 			List<String> itemsNeededList = Arrays.asList(itemsNeeded);
 		
-			if (itemsNeededList.contains(name))
+			if (itemsNeededList.contains(formattedName))
 			{
+				j++;
 				//click on Add to Card
 				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
-				break;
+				// 3 times
+				if (j == 3)
+				{
+					break;
+				}
 			}
 		}
 
