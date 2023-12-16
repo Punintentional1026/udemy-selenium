bee634b4631413d0552782d9aefee45089ea61a2
Rusty Horner <RustyHorner@gmail.com>
2023-12-15 19:07:09 -0500

    Course Section 8 - Video 75. Fixing the problem of inconsistent locators and making tests clean

--- Introduction/src/base.java
+++ Introduction/src/base.java
@@ -15,34 +15,31 @@ public class base {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
-		
+
 		Thread.sleep(3000L);
-		
-		String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot"}; 
-		
+
+		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot", "Carrot" };
+
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 
 		int j = 0;
 
-		for (int i=0; i < products.size(); i++)
-		{
-			//Brocolli - 1 Kg
+		for (int i = 0; i < products.size(); i++) {
+			// Brocolli - 1 Kg
 			String[] name = products.get(i).getText().split("-");
 			String formattedName = name[0].trim();
 			// format it to get actual vegetable name
 			// convert array into array list for easy search
 			// check whether name you extracted is present in array or not
-			
+
 			List<String> itemsNeededList = Arrays.asList(itemsNeeded);
-		
-			if (itemsNeededList.contains(formattedName))
-			{
+
+			if (itemsNeededList.contains(formattedName)) {
 				j++;
-				//click on Add to Card
-				driver.findElements(By.xpath("//button[text()='ADD TO CART']")).get(i).click();
+				// click on Add to Card
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
 				// 3 times
-				if (j == 3)
-				{
+				if (j == itemsNeeded.length) {
 					break;
 				}
 			}
