68393e5b831f7fef5271689bc9dd9dbfe8868138
Rusty Horner <RustyHorner@gmail.com>
2024-02-24 14:28:21 -0500

    Course Section 10 - Video 86. Handling Ajax/Mouse Interactions

---
+++ Introduction/src/actionsDemo.java
@@ -0,0 +1,23 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class actionsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.com/");
+		Actions a = new Actions(driver);
+		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+
+	}
+
+}
