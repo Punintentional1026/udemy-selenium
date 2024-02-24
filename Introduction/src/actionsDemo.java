2d29f6677006eada8cf5db8822777dc930583e6c
Rusty Horner <RustyHorner@gmail.com>
2024-02-24 14:41:01 -0500

    Course Section 10 - Video 87. Actions class-real time example

--- Introduction/src/actionsDemo.java
+++ Introduction/src/actionsDemo.java
@@ -1,7 +1,9 @@
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -16,7 +18,13 @@ public class actionsDemo {
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		Actions a = new Actions(driver);
-		a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+		WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
+		
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+		
+		//Moves to specific element
+		a.moveToElement(move).contextClick().build().perform();
+		//Drag and Drop
 
 	}
 
