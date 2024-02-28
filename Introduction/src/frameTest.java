cfc86a00b0e117fafea936034580a2e5e69672c7
Rusty Horner <RustyHorner@gmail.com>
2024-02-27 20:24:12 -0500

    Course Section 10 - Video 92. Frames Techniques-real time example

--- Introduction/src/frameTest.java
+++ Introduction/src/frameTest.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class frameTest {
 
@@ -12,10 +13,18 @@ public class frameTest {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://jqueryui.com/droppable/");
-		WebElement frame = driver.findElement(By.cssSelector("iframe.demo-frame"));
-		driver.switchTo().frame(frame);
-		driver.findElement(By.id("draggable")).click();
-		System.out.println(driver.findElement(By.id("draggable")).getText());
+		WebElement frame = driver.findElement(By.cssSelector("iframe[class='demo-frame']"));
+		//driver.switchTo().frame(frame);
+		System.out.println(driver.findElements(By.tagName("iframe")).size());
+		driver.switchTo().frame(0);
+		//driver.findElement(By.id("draggable")).click();
+
+		Actions a = new Actions(driver);
+		WebElement source = driver.findElement(By.id("draggable"));
+		WebElement target = driver.findElement(By.id("droppable"));
+		a.dragAndDrop(source, target).build().perform();
+		driver.switchTo().defaultContent();
+		driver.findElement(By.linkText("Accept")).click();
 
 	}
 
