bc6d99c6d95b88d7b6e4017bfe9a7d65090fc6d4
Rusty Horner <RustyHorner@gmail.com>
2024-03-07 20:10:08 -0500

    Course Section 11 - Video 96. Getting the Titles of child tabs with optimized while loop

--- Introduction/src/Scope.java
+++ Introduction/src/Scope.java
@@ -1,5 +1,7 @@
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -9,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scope {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		//1. Give me the count of links on the page
@@ -42,7 +44,32 @@ public class Scope {
 			String clickOnLinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
 			
 			columnDriver.findElements(By.tagName("a")).get(i).sendKeys(clickOnLinkTab);
+			//Instructor's solution for getting the window tab's title
+			Thread.sleep(5000L);
 		}
+
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		
+		while(it.hasNext())
+		{
+			driver.switchTo().window(it.next());
+			System.out.println(driver.getTitle());
+		}
+		
+		//This is my solution to get the each window tab's title
+//		Set<String> windows = driver.getWindowHandles();
+//		Iterator<String> it = windows.iterator();
+//		System.out.println(windows);
+//		System.out.println(it);
+//		while(it.hasNext())
+//		{
+//			driver.switchTo().window(it.next());
+//			Thread.sleep(5000);
+//			System.out.println(driver.getTitle());
+//		}
+//		Thread.sleep(5000);
+//		driver.switchTo().defaultContent();
 
 	}
 
