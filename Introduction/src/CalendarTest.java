43770a4e14e8e6e660b69119b193218b247d4e47
Rusty Horner <RustyHorner@gmail.com>
2024-04-28 15:51:58 -0400

    Course Section 11 - Video 99. Generic method to handle calendar with month and date

--- Introduction/src/CalendarTest.java
+++ Introduction/src/CalendarTest.java
@@ -1,8 +1,11 @@
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class CalendarTest {
 
@@ -11,6 +14,7 @@ public class CalendarTest {
 		String monthNumber = "6";
 		String date = "15";
 		String year = "2027";
+		String[] expectedList = {monthNumber, date, year};
 
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -36,6 +40,16 @@ public class CalendarTest {
 		driver.findElement(By.xpath("//button[text()='" + year + "']")).click();
 		driver.findElements(By.cssSelector(".react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber) - 1).click();
 		driver.findElement(By.xpath("//abbr[text()='" + date + "']")).click();
+
+		List<WebElement> actualList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+		
+		for(int i = 0; i < actualList.size(); i++)
+		{
+			System.out.println(actualList.get(i).getAttribute("value"));
+			Assert.assertEquals(actualList.get(i).getAttribute("value"), expectedList[i+1]);
+		}
+		
+		driver.close();
 	}
 
 }
