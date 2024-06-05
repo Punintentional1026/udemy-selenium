e3b093bed07777fb9bd0d5c962b8e5990a0b6da8
Rusty Horner <RustyHorner@gmail.com>
2024-06-04 21:01:21 -0400

    Course Section 13 - Video 110. Open Connection method to identify status codes of the links

--- Introduction/src/BrokenLinks.java
+++ Introduction/src/BrokenLinks.java
@@ -1,15 +1,17 @@
-import java.util.List;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrokenLinks {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException, IOException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedrive/chromedriver.exer");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
 		//broken URL
@@ -20,7 +22,13 @@ public class BrokenLinks {
 		
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		
-		String url = driver.findElement(By.cssSelector("a[href*='soapui']")).getAttribute("href");
+		String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+		
+		HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+		conn.setRequestMethod("HEAD");
+		conn.connect();
+		int respCode = conn.getResponseCode();
+		System.out.println(respCode);
 
 	}
 
