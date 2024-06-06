87cc27ebb24b1874759c2b5f9d8f8578be63258c
Rusty Horner <RustyHorner@gmail.com>
2024-06-06 19:59:57 -0400

    Course Section 13 - Video 111. Iterate over all links in the page to validate broken Links mechanism

--- Introduction/src/BrokenLinks.java
+++ Introduction/src/BrokenLinks.java
@@ -2,14 +2,17 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class BrokenLinks {
 
-	public static void main(String[] args) throws MalformedURLException, IOException {
+	public static void main(String[] args) throws MalformedURLException, IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
@@ -22,14 +25,24 @@ public class BrokenLinks {
 		
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 		
-		String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
 		
-		HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-		conn.setRequestMethod("HEAD");
-		conn.connect();
-		int respCode = conn.getResponseCode();
-		System.out.println(respCode);
+		for(WebElement link : links)
+		{
+			String url = link.getAttribute("href");
+			
+			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+			conn.setRequestMethod("HEAD");
+			conn.connect();
+			int respCode = conn.getResponseCode();
+			System.out.println(respCode);
 
+			if (respCode > 400)
+			{
+				System.out.println("The link with text " + link.getText() + " is broken with code " + respCode);
+				Assert.assertTrue(false);
+			}
+		}
 	}
 
 }
