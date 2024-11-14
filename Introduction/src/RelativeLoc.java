9cbd36d9869bcde56d29dab3308ec7bc756c42c7
Rusty Horner <RustyHorner@gmail.com>
2024-11-14 07:42:05 -0500

    Course Section 15 - Video 123. Relative locators with real time examples

--- Introduction/src/RelativeLoc.java
+++ Introduction/src/RelativeLoc.java
@@ -12,10 +12,18 @@ public class RelativeLoc {
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/angularpractice/");
-		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
 		
+		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
 		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+		
+		WebElement dateOfBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+		driver.findElement(with(By.tagName("input")).below(dateOfBirth)).click();
+		
+		WebElement iceCreamLabel = driver.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+		driver.findElement(with(By.tagName("input")).toLeftOf(iceCreamLabel)).click();
 
+		WebElement rdb = driver.findElement(By.id("inlineRadio1"));
+		System.out.println(driver.findElement(with(By.tagName("label")).toRightOf(rdb)).getText());
 	}
 
 }
