f142869826c53912f156b27256e2a78bc26b0cc4
Rusty Horner <RustyHorner@gmail.com>
2024-11-15 07:21:25 -0500

    Course Section 15 - Video 126. Taking WebElement Partial Screenshot with Selenium

--- Introduction/src/NewWindow.java
+++ Introduction/src/NewWindow.java
@@ -1,15 +1,20 @@
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import static org.openqa.selenium.support.locators.RelativeLocator.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 
 public class NewWindow {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Telum/Documents/GitHub/chromedriver/chromedriver.exe");
@@ -29,7 +34,11 @@ public class NewWindow {
 		.get(1).getText();
 		
 		driver.switchTo().window(parentWindowId);
-		driver.findElement(By.cssSelector("[name='name']")).sendKeys(courseName);
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		File file = name.getScreenshotAs(OutputType.FILE);
+		
+		FileUtils.copyFile(file, new File("logo.png"));
 		
 		driver.quit();
 
