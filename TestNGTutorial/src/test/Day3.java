98f218c6a7bc491c26c03549def16f3564039346
Rusty Horner <RustyHorner@gmail.com>
2024-11-16 10:04:22 -0500

    Course Section 16 - Video 132. Prioritizing the Testcases with TestNG

---
+++ TestNGTutorial/src/test/Day3.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day3 {
+	
+	@Test
+	public void WebLoginCarLoan()
+	{
+		//Selenium
+		System.out.println("WebLoginCar");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan()
+	{
+		//Appium
+		System.out.println("MobileLoginCar");
+	}
+	
+	@Test
+	public void LoginAPICarLoan()
+	{
+		//Rest API automation
+		System.out.println("APILoginCar");
+	}
+
+}
---
+++ TestNGTutorial/src/test/Day4.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Day4 {
+	
+	@Test
+	public void WebLoginHomeLoan()
+	{
+		//Selenium
+		System.out.println("WebLoginHome");
+	}
+	
+	@Test
+	public void MobileLoginHomeLoan()
+	{
+		//Appium
+		System.out.println("MobileLoginHome");
+	}
+	
+	@Test
+	public void LoginAPIHomeLoan()
+	{
+		//Rest API automation
+		System.out.println("APILoginHome");
+	}
+
+}
