397070fc6f170ccfa207ee5586f8b29bf8fd2edc
Rusty Horner <RustyHorner@gmail.com>
2024-11-16 11:10:11 -0500

    Course Section 16 - Video 135. TestNg Annotations part - 1

--- TestNGTutorial/src/test/Day1.java
+++ TestNGTutorial/src/test/Day1.java
@@ -1,8 +1,15 @@
 package test;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class Day1 {
+
+	@AfterTest
+	public void AfterTest()
+	{
+		System.out.println("I will execute last");
+	}
 
 	@Test
 	public void Demo()
--- TestNGTutorial/src/test/Day2.java
+++ TestNGTutorial/src/test/Day2.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Day2 {
@@ -8,6 +9,12 @@ public class Day2 {
 	public void ploan()
 	{
 		System.out.println("good");
+	}
+	
+	@BeforeTest
+	public void prerequisite()
+	{
+		System.out.println("I will execute first");
 	}
 
 }
