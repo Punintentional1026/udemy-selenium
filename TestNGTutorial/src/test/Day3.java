6f7760b2bf2ee2eba64c62936b4b24aeb0bf3436
Rusty Horner <RustyHorner@gmail.com>
2024-11-18 09:05:07 -0500

    Course Section 16 - Video 139. Parameterising from TestNG xml file

--- TestNGTutorial/src/test/Day3.java
+++ TestNGTutorial/src/test/Day3.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Day3 {
@@ -15,11 +16,13 @@ public class Day3 {
 		System.out.println("Before executing any method in the class");
 	}
 	
+	@Parameters({"URL"})
 	@Test
-	public void WebLoginCarLoan()
+	public void WebLoginCarLoan(String urlname)
 	{
 		//Selenium
 		System.out.println("WebLoginCar");
+		System.out.println(urlname);
 	}
 	
 	@BeforeMethod
--- TestNGTutorial/src/test/Day4.java
+++ TestNGTutorial/src/test/Day4.java
@@ -1,14 +1,17 @@
 package test;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Day4 {
 	
+	@Parameters({"URL"})
 	@Test
-	public void WebLoginHomeLoan()
+	public void WebLoginHomeLoan(String urlname)
 	{
 		//Selenium
-		System.out.println("WebLoginHome");
+		System.out.println("WebLoginHomePersonalLoan");
+		System.out.println(urlname);
 	}
 	
 	@Test(groups={"Smoke"})
