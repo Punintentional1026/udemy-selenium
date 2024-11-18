33ab56a72046395c949df5610c7d3441efbc6906
Rusty Horner <RustyHorner@gmail.com>
2024-11-18 07:55:27 -0500

    Course Section 16 - Video 137. Usage of Groups functionality in TestNG

--- TestNGTutorial/src/test/Day3.java
+++ TestNGTutorial/src/test/Day3.java
@@ -1,11 +1,19 @@
 package test;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Day3 {
+	
+	@BeforeClass
+	public void BeforeClass()
+	{
+		System.out.println("Before executing any method in the class");
+	}
 	
 	@Test
 	public void WebLoginCarLoan()
@@ -25,8 +33,14 @@ public class Day3 {
 	{
 		System.out.println("I will execute after every Day3 test class");
 	}
+
+	@AfterClass
+	public void AfterClass()
+	{
+		System.out.println("After executing all methods in the class");
+	}
 	
-	@Test
+	@Test(groups={"Smoke"})
 	public void MobileLoginCarLoan()
 	{
 		//Appium
