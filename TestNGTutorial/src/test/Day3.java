60fc82cd702b41a9b186b9fd74cf6f3aacf8f786
Rusty Horner <RustyHorner@gmail.com>
2024-11-19 07:07:23 -0500

    Course Section 16 - Video 141. DataProvider Annotation -Parameterizing Testcases-2

--- TestNGTutorial/src/test/Day3.java
+++ TestNGTutorial/src/test/Day3.java
@@ -65,11 +65,13 @@ public class Day3 {
 		System.out.println("Mobile SIGN IN");
 	}
 	
-	@Test(timeOut=4000)
-	public void MobileSignOutCarLoan()
+	@Test(dataProvider="getData")
+	public void MobileSignOutCarLoan(String username, String password)
 	{
 		//Appium
 		System.out.println("Mobile SIGN OUT");
+		System.out.println(username);
+		System.out.println(password);
 	}
 	
 	@Test(dependsOnMethods={"WebLoginCarLoan","MobileSignOutCarLoan"})
@@ -80,7 +82,7 @@ public class Day3 {
 	}
 	
 	@DataProvider
-	public void getData()
+	public Object[][] getData()
 	{
 		//1st combination - username password - good credit history = row
 		//2nd - username password - no credit history
@@ -88,7 +90,7 @@ public class Day3 {
 		Object[][] data = new Object[3][2];
 		//1st set
 		data[0][0] = "firstsetusername";
-		data[0][1] = "password";
+		data[0][1] = "firstpassword";
 		//columns in the row are nothing but values for that particular combination(row)
 		
 		//2nd set
@@ -99,6 +101,7 @@ public class Day3 {
 		data[2][0] = "thirdsetusername";
 		data[2][1] = "thirdpassword";
 		
+		return data;
 	}
 
 }
