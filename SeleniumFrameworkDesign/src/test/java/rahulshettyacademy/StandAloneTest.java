2c6323587faa0c87a7bc4a95b6b8626bec091a54
Rusty Horner <RustyHorner@gmail.com>
2024-12-28 15:51:38 -0500

    Course Section 18 - Video 157. Wrapping up end to end automation Script on Purchasing Order in Ecommerce App

--- SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/StandAloneTest.java
+++ SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/StandAloneTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -18,18 +19,19 @@ public class StandAloneTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String productName = "ZARA COAT 3";
+		String productName = "IPHONE 13 PRO";
 		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/client");
 		
 		driver.findElement(By.id("userEmail")).sendKeys("RobertKMaley@rhyta.com");
 		driver.findElement(By.id("userPassword")).sendKeys("CHl($$U-Vb\"0wq;");
 		driver.findElement(By.id("login")).click();
 
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".mb-3")));
 		
 		List<WebElement> products = driver.findElements(By.cssSelector(".mb-3"));
@@ -61,7 +63,22 @@ public class StandAloneTest {
 		
 		driver.findElement(By.cssSelector(".totalRow button")).click();
 
-
+		
+		//Select a country
+		//Click place order
+		//Go to next page
+		
+		Actions a = new Actions(driver);
+		a.sendKeys(driver.findElement(By.cssSelector("[placeholder='Select Country']")), "india").build().perform();
+		
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".ta-results")));
+		
+		driver.findElement(By.xpath("(//button[contains(@class, 'ta-item')])[2]")).click();
+		driver.findElement(By.cssSelector(".action__submit")).click();
+		
+		String confirmMessage = driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertTrue(confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		driver.close();
 	}
 
 }
