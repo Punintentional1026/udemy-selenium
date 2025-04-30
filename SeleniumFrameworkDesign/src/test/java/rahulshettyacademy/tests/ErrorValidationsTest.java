bc72f1fb8f9b829184ae1385e0aa627c17e6f37d
Rusty Horner <RustyHorner@gmail.com>
2025-04-30 08:18:45 -0400

    Course Section 20 - Video 169. Implement Test Strategy for Framework on how tests are divided based on modules

---
+++ SeleniumFrameworkDesign/src/test/java/rahulshettyacademy/tests/ErrorValidationsTest.java
@@ -0,0 +1,48 @@
+package rahulshettyacademy.tests;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import rahulshettyacademy.TestComponents.BaseTest;
+import rahulshettyacademy.pageobjects.CartPage;
+import rahulshettyacademy.pageobjects.CheckoutPage;
+import rahulshettyacademy.pageobjects.ConfirmationPage;
+import rahulshettyacademy.pageobjects.ProductCatalogue;
+
+public class ErrorValidationsTest extends BaseTest {
+
+
+	@Test
+	public void LoginErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+
+		landingPage.loginApplication("johnhsmith@testng.com", "4GWjvay3BuSciU1z");
+		Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());
+
+	}
+	
+
+	@Test
+	public void ProductErrorValidation() throws IOException, InterruptedException
+	{
+
+		String productName = "ZARA COAT 3";
+
+		ProductCatalogue productCatalogue = landingPage.loginApplication("johnhsmith@testng.com", "4GWjvay3BuSciU1z");
+		List<WebElement> products = productCatalogue.getProductList();
+		productCatalogue.addProductToCart(productName);
+
+		CartPage cartPage = productCatalogue.goToCartPage();
+		Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+		
+		Assert.assertFalse(match);
+
+	}
+
+}
